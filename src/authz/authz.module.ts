2830eb36b62941935daed9d495d97cda5240cbd2
Hantsy Bai <hantsy@gmail.com>
2021-05-20 16:09:29 +0800

    chore: clean codes

--- src/authz/authz.module.ts
+++ src/authz/authz.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { HasPermissionsGuard } from './has-permissions.guard';
+import { ConfigModule } from '@nestjs/config';
+import auth0Config from '../config/auth0.config';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    ConfigModule.forFeature(auth0Config),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [JwtStrategy, HasPermissionsGuard],
   exports: [PassportModule],
 })
